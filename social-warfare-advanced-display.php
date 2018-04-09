1bacfb97a36be51703322f68648a152bc7a025e1
ckmahoney <cortland.mahoney@gmail.com>
2018-04-09 13:50:13 -0400

    Refactored into a class and updated add_options().

--- social-warfare-advanced-display.php
+++ social-warfare-advanced-display.php
@@ -11,166 +11,100 @@
 
 defined( 'WPINC' ) || die;
 
-/**
- * Define plugin constants for use throughout the plugin (Version, Directories and Dependancies)
- */
-define( 'SWAD_VERSION' , '1.0.0' );
-define( 'SWAD_PLUGIN_FILE', __FILE__ );
-define( 'SWAD_PLUGIN_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
-define( 'SWAD_PLUGIN_DIR', dirname( __FILE__ ) );
-define( 'SWAD_ITEM_ID', 114481 );
-define( 'SWAD_CORE_VERSION_REQUIRED' , '2.3.2');
-if ( !defined( 'SWP_ACTIVATE_REGISTRATION_TAB' )) :
-    define( 'SWP_ACTIVATE_REGISTRATION_TAB', true );
-endif;
+add_action( 'plugins_loaded', function() {
+    class SWP_Advanced_Display extends SWP_Addon {
+        public function __construct() {
+            $this->name = 'Social Warfare - Advanced Display';
+            $this->key = 'advanced_display';
+            $this->product_id = 114481;
+            $this->version = '1.1.0';
+            $this->core_required = '3.0.0';
 
-
-/**
- * Add a registration key for the registration functions
- *
- * @param Array An array of registrations for each paid addon
- * @return Array An array modified to add this new registration key
- *
- */
-add_filter('swp_registrations' , 'social_warfare_enhanced_display_registration_key' , 20);
-function social_warfare_enhanced_display_registration_key($array) {
-
-    // Make sure core is on a version that contains our dependancies
-    if (defined('SWP_VERSION') && version_compare(SWP_VERSION , SWAD_CORE_VERSION_REQUIRED) >= 0){
-
-        // Add this plugin to the registrations array
-        $array['advanced_display'] = array(
-            'plugin_name' => 'Social Warfare - Advanced Display',
-            'key' => 'advanced_display',
-            'product_id' => SWAD_ITEM_ID
-        );
-    }
-
-    // Return the modified or unmodified array
-    return $array;
-}
-
-/**
- * A function to check for updates to this addon
- *
- * @since 1.0.0
- * @param none
- * @return none
- *
- */
-add_action( 'plugins_loaded' , 'swad_update_checker' , 20 );
-function swad_update_checker() {
-
-    // Make sure core is on a version that contains our dependancies
-    if (defined('SWP_VERSION') && version_compare(SWP_VERSION , SWAD_CORE_VERSION_REQUIRED) >= 0){
-
-        // Check if the plugin is registered
-        if( is_swp_addon_registered( 'advanced_display' ) ) {
-
-            // retrieve our license key from the DB
-            $license_key = swp_get_license_key('advanced_display');
-            $website_url = swp_get_site_url();
-
-            // setup the updater
-            $swad_updater = new SWP_Plugin_Updater( SWP_STORE_URL , __FILE__ , array(
-            	'version'   => SWAD_VERSION,		// current version number
-            	'license'   => $license_key,	// license key
-            	'item_id'   => SWAD_ITEM_ID,	// id of this plugin
-            	'author'    => 'Warfare Plugins',	// author of this plugin
-            	'url'       => $website_url,
-                'beta'      => false // set to true if you wish customers to receive update notifications of beta releases
-                )
-            );
-        }
-    }
-}
-
-/**
- * A function to add this plugins options to the options page
- *
- * @since  1.0.0
- * @param  array $swp_options The array of options
- * @return array $swp_options The modified array
- */
-add_filter('swp_options', 'SWAD_add_options' , 10 );
-function SWAD_add_options($swp_options) {
-
-    if (defined('SWP_VERSION') && version_compare(SWP_VERSION , SWAD_CORE_VERSION_REQUIRED) >= 0){
-
-        // Check if the plugin is registered
-        if( is_swp_addon_registered( 'advanced_display' ) ) {
-
-            $option['emphasize_icons'] = array(
-                'type'		=> 'select',
-                'size'		=> 'two-thirds',
-                'content'	=> array(
-                    '0' 	=> __( 'Don\'t Emphasize Any Buttons' ,'social-warfare' ),
-            		'1' 	=> __( 'Emphasize the First Button' ,'social-warfare' ),
-            		'2' 	=> __( 'Emphasize the First Two Buttons' ,'social-warfare' )
-            	),
-            	'default'	=> '0',
-            	'name'		=> __( 'Emphasize Buttons' ,'social-warfare' ),
-            	'premium'	=> true
-            );
-
-            $swp_options = swp_insert_option( $swp_options , 'swp_display' , 'buttons_divider' , $option , 'before' );
-
-        }
-    }
-
-    return $swp_options;
-}
-
-/**
- * The Button Emphasizer Function
- *
- * @since  1.0.0
- * @access public
- * @param  array $info An array of footer script information.
- * @return array $info A modified array of footer script information.
- */
-add_filter( 'swp_footer_scripts' , 'swp_emphasize_buttons' );
-function swp_emphasize_buttons( $info ) {
-
-    // Ensure that core is installed and that it is an updated/compatible version
-    if (defined('SWP_VERSION') && version_compare(SWP_VERSION , SWAD_CORE_VERSION_REQUIRED) >= 0){
-
-        // Check if the plugin is registered
-        if( is_swp_addon_registered( 'advanced_display' ) ) {
-
-        	$info['footer_output'] .= PHP_EOL . '
-                jQuery(window).on("pre_activate_buttons", swp_emphasize_buttons );
-                jQuery(window).on("floating_bar_revealed", swp_emphasize_buttons );
-
-                function swp_emphasize_buttons() {
-                    // *Disable on mobile devices.
-                    if (jQuery("body").width() < 576) return;
-
-                    var emphasize_icons = jQuery(".nc_socialPanel:not(.nc_socialPanelSide)").attr("data-emphasize");
-                    if( ! swp_isMobile.phone ) {
-                        setTimeout( function () {
-                            jQuery(".nc_socialPanel:not(.nc_socialPanelSide)").each(function(){
-                                var i = 1;
-                                jQuery(this).find(".nc_tweetContainer:not(.totes)").each(function(){
-                                    if(i <= emphasize_icons) {
-                                        jQuery(this).addClass("swp_nohover");
-                                        var term_width = jQuery(this).find(".swp_share").width();
-                                        var icon_width = jQuery(this).find("i.sw").outerWidth();
-                            			var container_width = jQuery(this).width();
-                            			var percentage_change = 1 + ((term_width + 35) / container_width);
-                                        jQuery(this).find(".iconFiller").width(term_width + icon_width + 25 + "px");
-                                        jQuery(this).css({flex:percentage_change + " 1 0%"});
-                                    }
-                                    ++i;
-                                });
-                            });
-                        } , 25 );
-                    }
+            if ( $this->is_registerd() ) {
+                if ( version_compare(SWP_VERSION, $this->core_required) >= 0) ) {
+                    add_filter( 'swp_options', [$this, 'add_options'], 1, 1 );
+                    add_filter( 'swp_footer_scripts', 'emphasize_buttons' );
+                } else {
+                    throw( "Please make sure you are using the most recent version of Social Warfare. We require at least version " . $this->core_required . "." );
                 }
-            ';
+            }
+        }
 
+
+        /**
+         * A function to add this plugins options to the options page
+         *
+         * @since  1.0.0
+         * @param  array $swp_options The array of options
+         * @return array $swp_options The modified array
+         */
+        function add_options( $swp_options ) {
+            global $SWP_Options_Page;
+
+            $emphasize_icons = new SWP_Select( __( 'Emphasize Buttons','social-warfare' ), 'emphasize_icons' );
+            $emphasize_icons->set_choices([
+                '0' 	=> __( 'Don\'t Emphasize Any Buttons','social-warfare' ),
+                '1' 	=> __( 'Emphasize the First Button','social-warfare' ),
+                '2' 	=> __( 'Emphasize the First Two Buttons','social-warfare' )
+              ])->set_priority( 100 )
+                ->set_size(  'sw-col-460' )
+                ->set_default( '0' )
+                ->set_premium( $this->key )
+
+            $SWP_Options_Page->tabs->display->sections->visual_options->add_option( $emphasize_icons );
+
+        }
+
+        /**
+         * The Button Emphasizer Function
+         *
+         * @since  1.0.0
+         * @access public
+         * @param  array $info An array of footer script information.
+         * @return array $info A modified array of footer script information.
+         */
+        function emphasize_buttons( $info ) {
+            ob_start();
+            ?>
+
+            jQuery(window).on("pre_activate_buttons", swp_emphasize_buttons );
+            jQuery(window).on("floating_bar_revealed", swp_emphasize_buttons );
+
+            function swp_emphasize_buttons() {
+                // *Disable on mobile devices.
+                if (jQuery("body").width() < 576) return;
+
+                var emphasize_icons = jQuery(".nc_socialPanel:not(.nc_socialPanelSide)").attr("data-emphasize");
+                if(!swp_isMobile.phone) {
+                    setTimeout(function() {
+                        jQuery(".nc_socialPanel:not(.nc_socialPanelSide)").each(function(){
+                            var i = 1;
+                            jQuery(this).find(".nc_tweetContainer:not(.totes)").each(function(){
+                                if(i <= emphasize_icons) {
+                                    jQuery(this).addClass("swp_nohover");
+                                    var term_width = jQuery(this).find(".swp_share").width();
+                                    var icon_width = jQuery(this).find("i.sw").outerWidth();
+                        			var container_width = jQuery(this).width();
+                        			var percentage_change = 1 + ((term_width + 35) / container_width);
+                                    jQuery(this).find(".iconFiller").width(term_width + icon_width + 25 + "px");
+                                    jQuery(this).css({flex:percentage_change + " 1 0%"});
+                                }
+                                ++i;
+                            });
+                        });
+                    }, 25 );
+                }
+            }
+
+            <?php
+            $script = ob_get_contents();
+            ob_end_clean();
+
+            $info['footer_output'] .= $script;
+
+            return $info;
         }
     }
 
-	return $info;
-}
+    new SWP_Advanced_Display();
+});
